4541531da39dcf5373ed4227c19d53de8b05007d
M4rshe1 <colin.heggli@gmail.com>
2025-06-12 16:37:30 +0200

    feat: add new tour reference for Frühling Alterswohnheim with details and links

--- lib/settings/tour-reference.ts
+++ lib/settings/tour-reference.ts
@@ -26,5 +26,18 @@ export const TOUR_REFERENCE = [
         },
         website: "https://www.bbz-sh.ch",
         tourLink: "https://tour.panoee.net/67e53dd02878e02586ee7206"
+    },
+    {
+        title: "Frühling Alterswohnheim",
+        description: "Das Alterswohnheim Frühling bietet älteren Menschen ein Zuhause mit vielfältigen Freizeit- und Betreuungsangeboten.",
+        image: "/images/services/tour/fruehling-alterswohnheim.jpg",
+        location: "Frühling Alterswohnheim Beringen",
+        tags: ["Altersheim", "Wohnen", "Betreuung"],
+        coordinates: {
+            latitude: 47.69571920962357,
+            longitude:  8.572579863715404,
+        },
+        website: "https://www.frueling.ch/index.php",
+        tourLink: "https://tour.panoee.net/684ada2bf2b32c5e880cd652"
     }
 ]
